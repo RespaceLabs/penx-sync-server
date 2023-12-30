24d9f6c22e1acf1c2216fe61002e75efe9fbdfde
0xzion <0xyz.penx@gmail.com>
2023-12-30 17:12:26 +0800

    feat: update port

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import Redis from "ioredis";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 4000;
 
 const redis = new Redis(process.env.REDIS_URL!);
 
